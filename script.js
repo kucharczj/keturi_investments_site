03d6253ea2720780b42b49f1f76e25c4f9dcca78
kucharczj <julia.kucharczyk1@gmail.com>
2025-05-14 13:53:41 -0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const hamburgerBtn = document.getElementById('hamburgerBtn');
-  const desktopNav = document.querySelector('.desktop-nav');
-
-  hamburgerBtn.addEventListener('click', () => {
-    desktopNav.classList.toggle('open');
-  });
-});
